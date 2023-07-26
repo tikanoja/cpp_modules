c4a6d2a4f80e598d24735133663f31b0da53c34f
Tuukka Tikanoja <ttikanoj@c2r5p1.hive.fi>
2023-07-26 16:01:24 +0300

    harl done

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -16,17 +16,35 @@ void Harl::debug(void){
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
-void info(void){
+void Harl::info(void){
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
-void warning(void){
+void Harl::warning(void){
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
-void error(void){
+void Harl::error(void){
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
-void complain(std::string level){
-	
+void Harl::complain(std::string level){
+	void (Harl::*function_ptr_array[4])(void);
+	function_ptr_array[0] = &Harl::debug;
+	function_ptr_array[1] = &Harl::info;
+	function_ptr_array[2] = &Harl::warning;
+	function_ptr_array[3] = &Harl::error;
+
+	std::string levels[4];
+	levels[0] = "debug";
+	levels[1] = "info";
+	levels[2] = "warning";
+	levels[3] = "error";
+
+	for (int index = 0; index < 4; index++){
+		if (levels[index] == level)
+		{
+			(this->*function_ptr_array[index])();
+			break;
+		}
+	}
 }
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,30 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttikanoj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/26 12:11:11 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/07/26 12:11:13 by ttikanoj         ###   ########.fr       */
+/*   Created: 2023/07/26 13:39:29 by ttikanoj          #+#    #+#             */
+/*   Updated: 2023/07/26 13:40:16 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL_HPP
-# define HARL_HPP
+#include "Harl.hpp"
 
-#include <string>
-#include <iostream>
+int main(void)
+{
+	Harl harl;
+	harl.complain("debug");
+	harl.complain("info");
+	harl.complain("warning");
+	harl.complain("error");
+	harl.complain("no complaints");
+	harl.complain("");
+	return (0);
+}
 
-class Harl{
-    private:
-        void debug(void);
-        void info(void);
-        void warning(void);
-        void error(void);
-
-    public:
-        void complain(std::string level);
-};
-
-#endif
