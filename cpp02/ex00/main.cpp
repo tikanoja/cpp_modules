c76991f9d6734e2f6039d1950ed4d3e1ac7fb00f
Tuukka Tikanoja <ttikanoj@c2r3p9.hive.fi>
2023-08-01 13:01:28 +0300

    all good and ready to hand in

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -20,5 +20,9 @@ int main( void ) {
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+
+    std::cout << "setting a new value of 42 for c" << std::endl;
+    c.setRawBits(42);
+    std::cout << c.getRawBits() << std::endl;
     return 0;
 }
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -14,9 +14,9 @@
 
 int main( void ) {
     Fixed a;
-    Fixed const b( 10 );
-    Fixed const c( 42.42f );
-    Fixed const d( b );
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
 
     a = Fixed( 1234.4321f );
 
@@ -29,6 +29,11 @@ int main( void ) {
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+    std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+    std::cout << "d is " << d.toFloat() << " as float" << std::endl;
     
     return 0;
 }
