07c1a0bceaf3b16f4a517b8862a92fe735b321f8
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-08-01 11:47:06 +0300

    clearer main

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:57:42 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/07/30 17:11:09 by tuukka           ###   ########.fr       */
+/*   Updated: 2023/08/01 09:22:24 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,48 @@
 const int Fixed::fractional_bits = 8;
 
 Fixed::Fixed(void){
-    std::cout << "Default constructor called" << std::endl;
+    //std::cout << "Default constructor called" << std::endl;
     this->fixed_point_value = 0;
     return ;
 }
 
 Fixed::Fixed(const int i){
-    std::cout << "Int constructor called" << std::endl;
+    //std::cout << "Int constructor called" << std::endl;
     this->fixed_point_value = i << this->fractional_bits;
     return ;
 }
 
 Fixed::Fixed(const float num){
-    std::cout << "Float constructor called" << std::endl;
+    //std::cout << "Float constructor called" << std::endl;
     this->fixed_point_value = std::roundf(num * (1 << this->fractional_bits));
     return ;
 }
 
 Fixed::Fixed(const Fixed &f){
-    std::cout << "Copy constructor called" << std::endl;
+    //std::cout << "Copy constructor called" << std::endl;
     *this = f;
     return ;
 }
 
 Fixed& Fixed::operator=(const Fixed& f){
-    std::cout << "Copy assignment operator called" << std::endl;
-    this->fixed_point_value = f.getRawBits();
+    //std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &f)
+        this->fixed_point_value = f.getRawBits();
     return (*this);
 }
 
 Fixed::~Fixed(void){
-    std::cout << "Destructor called" << std::endl;
+    //std::cout << "Destructor called" << std::endl;
     return ;
 }
 
 int Fixed::getRawBits(void) const{
-    std::cout << "getRawBits member function called" << std::endl;
+    //std::cout << "getRawBits member function called" << std::endl;
     return(this->fixed_point_value);
 }
 
 void Fixed::setRawBits(int const raw){
-    std::cout << "setRawBits member function called" << std::endl;
+    //std::cout << "setRawBits member function called" << std::endl;
     this->fixed_point_value = raw;
     return ;
 }
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -3,25 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:57:35 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/07/26 17:57:36 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/01 09:16:38 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void ) {
-    Fixed a;
-    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+// int main( void ) {
+//     Fixed a;
+//     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    //std::cout << Fixed::max( a, b ) << std::endl;
-    return 0;
+//     std::cout << a << std::endl;
+//     std::cout << ++a << std::endl;
+//     std::cout << a << std::endl;
+//     std::cout << a++ << std::endl;
+//     std::cout << a << std::endl;
+//     std::cout << b << std::endl;
+//     //std::cout << Fixed::max( a, b ) << std::endl;
+//     return 0;
+// }
+
+int main(void) {
+    Fixed a(2.5f);
+    Fixed b(4.75f);
+    Fixed c = a;
+
+    std::cout << "a is " << a << " and b is " << b << " and c is " << c << std::endl;
+
+    std::cout << std::endl << "Comparison" << std::endl;
+    std::cout << "a > b: " << (a > b) << std::endl;
+    std::cout << "a < b: " << (a < b) << std::endl;
+    std::cout << "a >= c: " << (a >= c) << std::endl;
+    std::cout << "a <= c: " << (a <= c) << std::endl;
+    std::cout << "a == c: " << (a == c) << std::endl;
+    std::cout << "a != b: " << (a != b) << std::endl;
+
+    std::cout << std::endl << "Arithmetic" << std::endl;
+    std::cout << "a + b: " << (a + b) << std::endl;
+    std::cout << "b - a: " << (b - a) << std::endl;
+    std::cout << "a * b: " << (a * b) << std::endl;
+    std::cout << "b / a: " << (b / a) << std::endl;
+
+    std::cout << std::endl << "Increment & Decrement" << std::endl;
+    std::cout << "a++: " << a++ << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "++a: " << ++a << std::endl;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "--b: " << --b << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "b--: " << b-- << std::endl;
+    std::cout << "b: " << b << std::endl;
+
+    std::cout << std::endl << "Min & Max" << std::endl;
+    Fixed smaller = Fixed::min(a, b);
+    const Fixed smallerConst = Fixed::min(a, b);
+    Fixed larger = Fixed::max(a, b);
+    const Fixed largerConst = Fixed::max(a, b);
+
+    std::cout << "Smaller (non-const): " << smaller << std::endl;
+    std::cout << "Smaller (const): " << smallerConst << std::endl;
+    std::cout << "Larger (non-const): " << larger << std::endl;
+    std::cout << "Larger (const): " << largerConst << std::endl;
+
+    return (0);
 }
