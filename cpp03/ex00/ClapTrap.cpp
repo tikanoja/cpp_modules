61b638bf92f15cdc6fbda6140e32f5a3d959f4c6
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-08-01 19:25:34 +0300

    ex00 done

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 13:53:05 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/01 13:53:07 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/01 19:23:42 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ ClapTrap::ClapTrap(void){
     this->hit_points = 10;
     this->energy_points = 10;
     this->attack_damage = 0;
+    return ;
 }
 
 ClapTrap::ClapTrap(std::string name){
@@ -26,12 +27,13 @@ ClapTrap::ClapTrap(std::string name){
     this->hit_points = 10;
     this->energy_points = 10;
     this->attack_damage = 0;
-
+    return ;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &copy){
     std::cout << "Copy constructor called" << std::endl;
     *this = copy;
+    return ;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& copy){
@@ -47,18 +49,46 @@ ClapTrap& ClapTrap::operator=(const ClapTrap& copy){
 
 ClapTrap::~ClapTrap(void){
     std::cout << "Destructor called" << std::endl;
+    return ;
 }
 
 void ClapTrap::attack(const std::string& target){
+    if (this->hit_points == 0){
+        std::cout << this->name << " is dead and unable to attack...!" << std::endl;
+        return ;
+    } else if (this->energy_points == 0){
+        std::cout << this->name << " is out of energy and cannot attack...!" << std::endl;
+        return ;
+    }
     std::cout << this->name << " attacks " << target << ", causing " << this->attack_damage << " points of damage!" << std::endl;
+    this->energy_points--;
+    return ;
 }
 
 void ClapTrap::takeDamage(unsigned int amount){
+    if (this->hit_points == 0){
+        std::cout << this->name << " is already dead! No need for any more violence!" << std::endl;
+        return ;
+    } else if (amount > (unsigned int)this->hit_points)
+        amount = this->hit_points;
     std::cout << this->name << " takes " << amount << " points of damage!" << std::endl;
     this->hit_points -= amount;
+    if (this->hit_points == 0)
+        std::cout << this->name << " is dead!" << std::endl;
+    return ;
 }
 
 void ClapTrap::beRepaired(unsigned int amount){
+    if (this->hit_points == 0){
+        std::cout << this->name << " is dead and unable to repair...!" << std::endl;
+        return ;
+    } else if (this->energy_points == 0){
+        std::cout << this->name << " is out of energy and cannot repair...!" << std::endl;
+        return ;
+    } else if (amount > (unsigned int)(INT_MAX - this->hit_points))
+        amount = INT_MAX - this->hit_points;
     std::cout << this->name << " repairs for " << amount << " points of health!" << std::endl;
     this->hit_points += amount;
+    this->energy_points--;
+    return ;
 }
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 13:53:19 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/01 13:53:20 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/01 19:21:18 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@ int main(void){
     robotti.attack("Iines");
     robotti.takeDamage(7);
     robotti.beRepaired(10);
+    robotti.takeDamage(4);
+    robotti.beRepaired(1111155);
+    robotti.beRepaired(2147483650);
+    robotti.takeDamage(2147483650);
+    robotti.attack("Iines");
     return (0);
 }
