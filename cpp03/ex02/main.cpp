78ba899d4be1f935d234995c09ca225e41bbf759
Tuukka Tikanoja <ttikanoj@c2r5p1.hive.fi>
2023-08-04 09:39:23 +0300

    ex02 ready to hand in

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -15,26 +15,11 @@
 #include "FragTrap.hpp"
 
 int main(void){
-    ClapTrap clap("Clap");
-    ScavTrap scav("Scav");
     FragTrap frag("Frag");
+    ScavTrap scav("Scav");
 
-    clap.attack("your mom");
-    scav.attack("some kind of goblin");
-    clap.takeDamage(5);
-    scav.takeDamage(5);
-    clap.beRepaired(5);
-    scav.beRepaired(5);
-    scav.guardGate();
-    // for (int i = 0; i < 9; i++){
-    //     clap.attack("an annoying mosquito");
-    // }
-    // for (int i = 0; i < 49; i++){
-    //     scav.attack("the goblin king");
-    // }
-    clap.takeDamage(200);
-    scav.takeDamage(200);
+    frag.takeDamage(1);
+    frag.attack("the patriarchy");
     frag.highFivesGuys();
-    //as the objects are on the stack, we'll start destroying them LIFO
     return (0);
 }
