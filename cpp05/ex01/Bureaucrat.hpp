1ad55ba29757a912c3f28790e0ca919ddc771fd1
Tuukka Tikanoja <ttikanoj@c3r5p6.hive.fi>
2023-08-17 10:13:42 +0300

    init ex01

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 10:22:15 by ttikanoj          #+#    #+#             */
+/*   Updated: 2023/08/17 10:12:26 by ttikanoj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Bureaucrat {
+	private:
+		std::string const name;
+		int grade;
+
+	public:
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(const Bureaucrat &copy);
+		Bureaucrat& operator=(const Bureaucrat &copy);
+		~Bureaucrat(void);
+
+		std::string getName(void) const;
+		int getGrade(void) const;
+		void increaseGrade(void);
+		void decreaseGrade(void);
+
+		class GradeTooHighException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+				const char* what() const throw();
+		};
+};
+
+std::ostream& operator<<(std::ostream &output, const Bureaucrat &b);
+
+#endif
+
