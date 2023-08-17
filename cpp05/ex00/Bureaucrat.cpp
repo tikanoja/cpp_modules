c97e8d9f3378811ea0400a97342174f7047c3c05
Tuukka Tikanoja <ttikanoj@c3r5p6.hive.fi>
2023-08-17 10:11:54 +0300

    indent to tabs for swag

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,98 +6,98 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:22:23 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/17 09:51:09 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/17 10:10:09 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(std::string const newName, int newGrade) : name(newName) {
-    std::cout << "Bureaucrat constructor called for officer " << newName << ", grade " << newGrade << std::endl;
-    try {
-        if (newGrade < 1)
-            throw Bureaucrat::GradeTooHighException();
-        else if (newGrade > 150)
-            throw Bureaucrat::GradeTooLowException();
-        this->grade = newGrade;
-    } catch (std::exception& e) {
-        std::cout << "Caught an error in constructor: " << e.what() << std::endl;
-        std::cout << "Substituting grade " << newGrade << " with the closest value in range: ";
-        if (newGrade < 1) {
-            std::cout << "1." << std::endl;
-            this->grade = 1;
-        } else {
-            std::cout << "150." << std::endl;
-            this->grade = 150;
-        }
-    }
-    return ;
+	std::cout << "Bureaucrat constructor called for officer " << newName << ", grade " << newGrade << std::endl;
+	try {
+		if (newGrade < 1)
+			throw Bureaucrat::GradeTooHighException();
+		else if (newGrade > 150)
+			throw Bureaucrat::GradeTooLowException();
+		this->grade = newGrade;
+	} catch (std::exception& e) {
+		std::cout << "Caught an error in constructor: " << e.what() << std::endl;
+		std::cout << "Substituting grade " << newGrade << " with the closest value in range: ";
+		if (newGrade < 1) {
+			std::cout << "1." << std::endl;
+			this->grade = 1;
+		} else {
+			std::cout << "150." << std::endl;
+			this->grade = 150;
+		}
+	}
+	return ;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &copy) : name(copy.getName()) {
-    std::cout << "Bureaucrat copy constructor called" << std::endl;
-    *this = copy;
-    return ;
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+	*this = copy;
+	return ;
 }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat &copy) {
-    std::cout << "Bureaucrat copy assignment constructor called" << std::endl;
-    if (this != &copy){
-        this->grade = copy.getGrade();
-    }
-    return (*this);
+	std::cout << "Bureaucrat copy assignment constructor called" << std::endl;
+	if (this != &copy){
+		this->grade = copy.getGrade();
+	}
+	return (*this);
 }
 
 Bureaucrat::~Bureaucrat(void) {
-    std::cout << "Bureaucrat destructor called" << std::endl;
-    return ;
+	std::cout << "Bureaucrat destructor called" << std::endl;
+	return ;
 }
 
 std::string Bureaucrat::getName(void) const {
-    return (this->name);
+	return (this->name);
 }
 
 int Bureaucrat::getGrade(void) const {
-    return (this->grade);
+	return (this->grade);
 }
 
 void Bureaucrat::increaseGrade(void) {
-    try {
-        if (this->grade > 1) {
-            this->grade--;
-            std::cout << this->getName() << " has been promoted to grade " << this->getGrade() << "!" << std::endl;
-        }
-        else
-            throw Bureaucrat::GradeTooHighException();
-    } catch (std::exception& e) {
-        std::cout << "Caught an error increasing grade: " << e.what() <<  std::endl;
-    }
-    return ;
+	try {
+		if (this->grade > 1) {
+			this->grade--;
+			std::cout << this->getName() << " has been promoted to grade " << this->getGrade() << "!" << std::endl;
+		}
+		else
+			throw Bureaucrat::GradeTooHighException();
+	} catch (std::exception& e) {
+		std::cout << "Caught an error increasing grade: " << e.what() <<  std::endl;
+	}
+	return ;
 }
 
 void Bureaucrat::decreaseGrade(void) {
-    try {
-        if (this->grade < 150) {
-            this->grade++;
-            std::cout << this->getName() << " has been demoted to grade " << this->getGrade() << "..." << std::endl;
-        }
-        else
-            throw Bureaucrat::GradeTooLowException();
-    } catch (std::exception& e) {
-        std::cout << "Caught an error decreasing grade: " << e.what() <<  std::endl;
-    }
-    return ;
+	try {
+		if (this->grade < 150) {
+			this->grade++;
+			std::cout << this->getName() << " has been demoted to grade " << this->getGrade() << "..." << std::endl;
+		}
+		else
+			throw Bureaucrat::GradeTooLowException();
+	} catch (std::exception& e) {
+		std::cout << "Caught an error decreasing grade: " << e.what() <<  std::endl;
+	}
+	return ;
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-    return ("Grade too high!\0");
+	return ("Grade too high!\0");
 }
 
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-    return ("Grade too low!\0");
+	return ("Grade too low!\0");
 }
 
 std::ostream& operator<<(std::ostream &output, const Bureaucrat &b) {
-    output << b.getName() << ", " << b.getGrade();
-    return (output);
+	output << b.getName() << ", " << b.getGrade();
+	return (output);
 }
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,38 +6,38 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 10:22:03 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/17 09:52:21 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/17 10:10:31 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
-    std::cout << "INSTANTIATING Jim WITH LOW GRADE" << std::endl;
-    Bureaucrat jim("Jim", 999999999);
-    jim.decreaseGrade();
-    jim.increaseGrade();
-    std::cout << jim << std::endl;
-    std::cout << std::endl;
+	std::cout << "INSTANTIATING Jim WITH LOW GRADE" << std::endl;
+	Bureaucrat jim("Jim", 999999999);
+	jim.decreaseGrade();
+	jim.increaseGrade();
+	std::cout << jim << std::endl;
+	std::cout << std::endl;
 
-    std::cout << "INSTANTIATING Tim WITH HIGH GRADE" << std::endl;
-    Bureaucrat tim("Tim", -100000);
-    tim.increaseGrade();
-    tim.decreaseGrade();
-    std::cout << tim << std::endl;
-    std::cout << std::endl;
+	std::cout << "INSTANTIATING Tim WITH HIGH GRADE" << std::endl;
+	Bureaucrat tim("Tim", -100000);
+	tim.increaseGrade();
+	tim.decreaseGrade();
+	std::cout << tim << std::endl;
+	std::cout << std::endl;
 
-    std::cout << "INSTANTIATING WITH Bureaucrat james(jim);" << std::endl;
-    Bureaucrat james(jim);
-    james.increaseGrade();
-    std::cout << james << std::endl;
-    std::cout << std::endl;
+	std::cout << "INSTANTIATING WITH Bureaucrat james(jim);" << std::endl;
+	Bureaucrat james(jim);
+	james.increaseGrade();
+	std::cout << james << std::endl;
+	std::cout << std::endl;
 
-    std::cout << "INSTANTIATING WITH Bureaucrat john = james;" << std::endl;
-    Bureaucrat john = james;
-    john.increaseGrade();
-    std::cout << john << std::endl;
-    std::cout << std::endl;
-    
-    return 0;
+	std::cout << "INSTANTIATING WITH Bureaucrat john = james;" << std::endl;
+	Bureaucrat john = james;
+	john.increaseGrade();
+	std::cout << john << std::endl;
+	std::cout << std::endl;
+	
+	return 0;
 }
