799b4064fd81b028ac8a67da2916431988fa7a9c
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-08-23 12:27:07 +0300

    added constructors

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,2 +1,26 @@
 
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(void) {
+	std::cout << "ScalarConverter constructor called" << std::endl;
+	return ;
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &copy) {
+	std::cout << "ScalarConverter copy constructor called" << std::endl;
+	*this = copy;
+	return ;
+}
+
+//muista täyttää tähä oikeet valuet jos tulee jotaa private members
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter &copy) {
+	std::cout << "ScalarConverter copy assignment constructor called" << std::endl;
+	if (this != &copy)
+		this->foo = copy.foo;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter(void) {
+	std::cout << "ScalarConverter destructor called" << std::endl;
+	return ;
+}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -2,9 +2,18 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+#include <iostream>
+#include <string>
+
 //"make a static class ????"
 class ScalarConverter {
-
+	private:
+		int foo; // ota tää helvettii
+	public:
+		ScalarConverter(void);
+		ScalarConverter(const ScalarConverter &copy);
+		ScalarConverter& operator=(const ScalarConverter &copy);
+		~ScalarConverter(void);
 };
 
 #endif
