8b64675a62814601beae53fac7bbc8f9e90d1c7f
Tuukka Tikanoja <ttikanoj@c3r5p6.hive.fi>
2023-08-28 14:49:16 +0300

    init ex01

---
+++ cpp06/ex01/Serializer.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 14:45:51 by ttikanoj          #+#    #+#             */
+/*   Updated: 2023/08/28 14:47:25 by ttikanoj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+//reinterpret_cast
+class Serializer {
+	private:
+	
+	
+	public:
+	
+	
+};
+
+#endif
