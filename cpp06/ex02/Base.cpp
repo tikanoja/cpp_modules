fce061308918feb47e5e71734fea16953ef99067
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-08-28 20:43:28 +0300

    init ex02

---
+++ cpp06/ex02/Base.cpp
@@ -0,0 +1,18 @@
+#include "Base.hpp"
+
+Base::~Base() {
+	return ;
+}
+
+Base* Base::generate(void) {
+	Base* ptr;
+	return (ptr);
+}
+
+void Base::identify(Base* p) {
+	return ;
+}
+
+void Base::identify(Base& p) {
+	return ;
+}
---
+++ cpp06/ex02/Base.hpp
@@ -0,0 +1,17 @@
+#ifndef BASE_HPP
+# define BASE_HPP
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+class Base {
+	private:
+	public:
+		virtual ~Base();
+		Base* generate(void);
+		void identify(Base* p);
+		void identify(Base& p);
+};
+
+#endif
