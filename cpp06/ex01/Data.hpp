e73d6f6b36a369621d9ad652ba94ac31040c7e4d
Tuukka Tikanoja <ttikanoj@c3r5p6.hive.fi>
2023-08-28 15:01:55 +0300

    added data struct

--- cpp06/ex01/Data.hpp
+++ cpp06/ex01/Data.hpp
@@ -1,30 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Serializer.hpp                                     :+:      :+:    :+:   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/28 14:45:51 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/28 15:01:28 by ttikanoj         ###   ########.fr       */
+/*   Created: 2023/08/28 14:59:42 by ttikanoj          #+#    #+#             */
+/*   Updated: 2023/08/28 15:01:27 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SERIALIZER_HPP
-# define SERIALIZER_HPP
-
-#include <stdint.h>
-#include "Data.hpp"
-
-//reinterpret_cast
-class Serializer {
-	private:
-	
-	
-	public:
-		uintptr_t serializer(Data* ptr);
-		Data* deserialize(uintptr_t raw);
-	
+struct Data {
+	int foo;
 };
-
-#endif
