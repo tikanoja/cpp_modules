e8653845c4dfe341c2c0737d50afa762ed159edc
tikanoja <107203742+tikanoja@users.noreply.github.com>
2023-08-31 17:30:29 +0300

    ex01 workinggg

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 10:42:52 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/31 14:05:18 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:29:59 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 # define ITER_HPP
 #include <iostream>
 
-template<typename T, typename L, typename A>
-void iter(T& arr, L arrlen, void (*func)(A)) {
-	for (int i = 0; i < arrlen, i++;) {
+template <typename T>
+void printer(T arg) {
+	std::cout << arg << std::endl;
+	return ;
+}
+
+template <typename T>
+void plusone(T& arg) {
+	arg++;
+	return ;
+}
+
+template <typename T>
+void iter(T* arr, int arrlen, void (*func)(T& arg)) {
+	for (int i = 0; i < arrlen; i++) {
 		func(arr[i]);
 	}
 	return ;
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 10:41:17 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/31 14:04:35 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:29:58 by tuukka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,24 @@ void plusone(int &i) {
 	return ;
 }
 
-void printer(std::string str) {
-	std::cout << str << std::endl;
-	return ;
-}
-
 int main(void) {
-	// int foo[] = {0, 1, 2, 3, 4};
+	int foo[] = {0, 1, 2, 3, 4};
 	char bar[] = {'a', 'b', 'c', 'd', 'e'};
-	// std::string stringsss[] = {"bart", "lisa", "maggie", "marge", "homer"};
-	
-	iter(bar, 5, &printer);
+	std::string strs[] = {"bart", "lisa", "maggie", "marge", "homer"};
+	std::cout << "calling printer() on int array foo[]" << std::endl;
+	iter(foo, 5, printer);
+	std::cout << "calling plusone() on int array foo[]" << std::endl;
+	iter(foo, 5, plusone);
+	std::cout << "calling printer() on int array foo[]" << std::endl;
+	iter(foo, 5, printer);
+	std::cout << "calling printer() on char array bar[]" << std::endl;
+	iter(bar, 5, printer);
+	std::cout << "calling plusone() on char array foo[]" << std::endl;
+	iter(bar, 5, plusone);
+	std::cout << "calling printer() on char array bar[]" << std::endl;
+	iter(bar, 5, printer);
+	std::cout << "calling printer() on string array strs[]" << std::endl;
+	iter(strs, 5, printer);
 
 	return (0);
 }
