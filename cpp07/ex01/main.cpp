79f7e951e8e8486569926c4c50feba7b777fc68e
Tuukka Tikanoja <ttikanoj@c3r3p6.hive.fi>
2023-09-01 10:54:26 +0300

    more nice tests for ex01

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tuukka <tuukka@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 10:41:17 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/08/31 17:29:58 by tuukka           ###   ########.fr       */
+/*   Updated: 2023/09/01 10:54:02 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,38 @@
 #include <string>
 #include <iostream>
 
-void plusone(int &i) {
-	i++;
-	return ;
-}
-
 int main(void) {
 	int foo[] = {0, 1, 2, 3, 4};
 	char bar[] = {'a', 'b', 'c', 'd', 'e'};
+	float floarr[] = {0.0f, 1.1f, 2.2f, 3.3f, 4.4f};
+	double doubarr[] = {0.0, 100.11, 200.22, 300.33, 400.44};
 	std::string strs[] = {"bart", "lisa", "maggie", "marge", "homer"};
-	std::cout << "calling printer() on int array foo[]" << std::endl;
-	iter(foo, 5, printer);
+
+	std::cout << "* * * * * INT ARRAY * * * * *" << std::endl;
 	std::cout << "calling plusone() on int array foo[]" << std::endl;
 	iter(foo, 5, plusone);
 	std::cout << "calling printer() on int array foo[]" << std::endl;
 	iter(foo, 5, printer);
-	std::cout << "calling printer() on char array bar[]" << std::endl;
-	iter(bar, 5, printer);
-	std::cout << "calling plusone() on char array foo[]" << std::endl;
+
+	std::cout << std::endl << "* * * * * CHAR ARRAY * * * * *" << std::endl;
+	std::cout << "calling plusone() on char array bar[]" << std::endl;
 	iter(bar, 5, plusone);
 	std::cout << "calling printer() on char array bar[]" << std::endl;
 	iter(bar, 5, printer);
+
+	std::cout << std::endl << "* * * * * FLOAT ARRAY * * * * *" << std::endl;
+	std::cout << "calling plusone() on float array floarr[]" << std::endl;
+	iter(floarr, 5, plusone);
+	std::cout << "calling printer() on float array floarr[]" << std::endl;
+	iter(floarr, 5, printer);
+
+	std::cout << std::endl << "* * * * * DOUBLE ARRAY * * * * *" << std::endl;
+	std::cout << "calling plusone() on double array doubarr[]" << std::endl;
+	iter(doubarr, 5, plusone);
+	std::cout << "calling printer() on double array doubarr[]" << std::endl;
+	iter(doubarr, 5, printer);
+
+	std::cout << std::endl << "* * * * * STRING ARRAY * * * * *" << std::endl;
 	std::cout << "calling printer() on string array strs[]" << std::endl;
 	iter(strs, 5, printer);
 
