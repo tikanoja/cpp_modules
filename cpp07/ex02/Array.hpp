cc8cba747fff47da0c16ba4050c5bdaaafae8bf0
Tuukka Tikanoja <ttikanoj@c3r3p6.hive.fi>
2023-09-01 12:55:30 +0300

    working on the constructor templates

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 10:56:37 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/09/01 12:29:10 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/09/01 12:54:55 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ class Array {
 		T* arr;
 
 	public:
-		Array(); //creates an empty array
+		Array(void); //creates an empty array
 		Array(unsigned int n); //creates an array of n elements *Tip: Try to compile int * a = new int(); then display *a.* initialized my default ???
-		~Array(); //remember to check mem leaks
+		~Array(void); //remember to check mem leaks
 		Array(Array const& other); //deep copy! modifying the original arr must not affect the copy & vice versa!
 
 		Array& operator=(Array const& other); //deep copy! modifying the original arr must not affect the copy & vice versa!
@@ -38,13 +38,40 @@ class Array {
 };
 
 template <typename T>
-const char* Array<T>::OutOfBounds::what() const throw() {
-	return ("You are trying to access non-allocated memory! :(\0");
+Array<T>::Array(void) {
+	std::cout << "Array(void) constructor called" << std::endl;
+	size = 0;
+	arr = new T[0];
+	return ;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) {
+	std::cout << "Array(unsigned int n) constructor called" << std::endl;
+	size = n;
+	arr = new T[n];
+	return ;
+}
+
+template <typename T>
+Array<T>::Array(Array const& other) {
+	std::cout << "Array copy constructor called" << std::endl;
+	size = other.size;
+	delete[] arr; //delete possibly allocated memory before copying the previous memory over!
+	arr = new T[size];
+	for (unsigned int i = 0; other.arr[i]; i++)
+		arr[i] = other.arr[i];
+	return ;
 }
 
 template <typename T>
 unsigned int Array<T>::size(void) const { //onkohan const tarpeen ?
 	return (this->size); //onkohan toi this-> tarpeellinen?
+}
+
+template <typename T>
+const char* Array<T>::OutOfBounds::what() const throw() {
+	return ("You are trying to access non-allocated memory! :(\0");
 }
 
 #endif
