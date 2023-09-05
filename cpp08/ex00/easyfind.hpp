042a137087361cd10cd8a74b191e3e7d513acd54
Tuukka Tikanoja <ttikanoj@c2r5p1.hive.fi>
2023-09-05 09:43:43 +0300

    little improvements to hpp and main

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,13 +6,20 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:03:03 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/09/04 16:38:27 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/09/05 09:43:13 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <algorithm>
+#include <array>
+
 template <typename T>
 void easyfind(T haystack, int needle) {
-	(void)haystack;
-	(void)needle;
+	T::iterator it = std::find(haystack.begin(), haystack.end(), needle);
+	if (it != haystack.end())
+		std::cout << "Found " << needle << " in position " << std::distance(haystack.begin(), it) << "!" << std::endl;
+	else
+		std::cout << "Can't find it..." << std::endl;
 	return ;
 }
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,30 +6,46 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:03:07 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/09/04 17:09:34 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/09/05 09:40:22 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <array> //std::array
+#include <vector>
 #include <algorithm> //std::fill
 #include <iostream>
 
 //you dont have to handle associative containers eg. std::map
 //which implies that you DO have to handle standard (sequential) containers!
 //eg. std::vector, std::list, std::deque, std::array
+
 int main(void) {
 	std::array<int, 1000> intarray;
-	// for (std::array<int, 1000>::iterator i = intarray.begin();\
-	// 	i != intarray.end();\
-	// 	i++) { // 1. declaring iterator and setting to the start of the container 2. terminating contition is the end of the container 3. increment of the iterator, maybe should be ++i
-	// 	*i = rand(); //iterator itself is a pointer to the location in the array;
-	// }
-	int number = 0;
-	std::fill(intarray.begin(), intarray.end(), number++);
-	for (int i = 0; i < 1000; i++) {
-		std::cout << intarray[i] << std::endl;
+	int num = 0;
+	for (std::array<int, 1000>::iterator i = intarray.begin();\
+		i != intarray.end();\
+		i++) { // 1. declaring iterator and setting to the start of the container 2. terminating contition is the end of the container 3. increment of the iterator, maybe should be ++i
+		*i = num; //iterator itself is a pointer to the location in the array;
+		num++;
 	}
-	// intarray.data(); //returns a raw pointer to the array
+	easyfind(intarray, 42);
+	easyfind(intarray, 999);
+	easyfind(intarray, 1000);
+
+	std::vector<int> intvector;
+	intvector.push_back(105);
+	intvector.push_back(666);
+	intvector.push_back(404);
+	intvector.push_back(42);
+	intvector.push_back(1337);
+	easyfind(intarray, 42);
+	easyfind(intarray, -1);
+	easyfind(intarray, 1338);
+
 	return (0);
 }
+
+	// bretty cool!
+	// std::fill(intarray.begin(), intarray.end(), number++);
+	// intarray.data(); //returns a raw pointer to the array
