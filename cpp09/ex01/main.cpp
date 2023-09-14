496055053933d6666a7a9e208ff0eb77292ffde0
Tuukka Tikanoja <ttikanoj@c2r5p1.hive.fi>
2023-09-14 15:31:49 +0300

    init pt 2

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,9 +6,18 @@
 /*   By: ttikanoj <ttikanoj@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 15:24:25 by ttikanoj          #+#    #+#             */
-/*   Updated: 2023/09/14 15:26:30 by ttikanoj         ###   ########.fr       */
+/*   Updated: 2023/09/14 15:30:40 by ttikanoj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+#include <iostream>
 
+int main(int ac, char** av) {
+	if (ac != 2) {
+		std::cerr << "usage: ./RPN \"8 9 * 9 - 9 - 9 - 4 - 1 +\"" << std::endl;
+		return (1);
+	}
+	(void)av;
+	return (0);
+}
